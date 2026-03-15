150dd66d4808df53aed523a8c8cd85ceaf40e597
subashvs7 <subash07070707@gmail.com>
2026-03-15 20:40:17 +0530

    first

---
+++ backend/config/cors.php
@@ -0,0 +1,11 @@
+<php 
+
+return['paths' => ['api/*'],
+'allowed_origins' => ['http://localhost:5173'],  // React Vite default port
+'allowed_methods' => ['*'],
+'allowed_headers' => ['*'],
+]
+
+
+
+?>
